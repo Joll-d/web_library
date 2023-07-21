fbf9a1f6965d510c2606dd54aeb88d28eea32af4
Andrii Smaliuha <alkpppp506@gmail.com>
2023-07-21 19:40:09 +0200

    Refactored Translator

--- libraryJ/utils/translator.py
+++ libraryJ/utils/translator.py
@@ -1,4 +1,5 @@
 from deep_translator import GoogleTranslator
+import asyncio
 
 class Translator:
 
@@ -9,12 +10,21 @@ class Translator:
         if isinstance(source_text, str):
             translated_text = self.translate_element(source_text)
         elif isinstance(source_text, list):
+            loop = asyncio.new_event_loop() 
+            asyncio.set_event_loop(loop) 
+
             translated_text = source_text.copy()
-            for i, source_text_element in enumerate(translated_text):
-                translated_text[i] = self.translate_element(source_text_element)
-                print(i)
+            tasks = [self.translate_element_async(element) for element in source_text]
+            translated_text = loop.run_until_complete(asyncio.gather(*tasks))
+
+            loop.close() 
+
         return translated_text
 
+    async def translate_element_async(self, source_text: str) -> str:
+        translated_text = GoogleTranslator(source='auto', target=self.destination_language).translate(source_text)
+        return translated_text
+    
     def translate_element(self, source_text: str) -> str:
         translated_text = GoogleTranslator(source='auto', target=self.destination_language).translate(source_text)
         return translated_text
